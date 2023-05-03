8f3612ca0871ce203a6e659add783c32b4fb651b
Rhylei <Rhylei@aureate.dev>
2023-05-03 21:55:07 +1200

    finsished assessment

--- Scripts/AssisgnmentQueries.sql
+++ Scripts/AssisgnmentQueries.sql
@@ -87,7 +87,8 @@ from customer;
 
 
 -- Query 12
-select count(product_id) as total, min(product_price) as minimum, max(product_price) as maximum, avg(product_price) as average
+select count(product_id) as total,
+	   min(product_price) as minimum, max(product_price) as maximum, avg(product_price) as average
 from product;
 
 
@@ -96,7 +97,7 @@ from product;
 -- Query 13
 select v.vendor_name, p.product_name, p.product_price
 from product p
-join vendor v on v.vendor_id = p.vendor_id;
+	join vendor v on v.vendor_id = p.vendor_id;
 
 
 
@@ -104,8 +105,8 @@ join vendor v on v.vendor_id = p.vendor_id;
 -- Query 14
 select p.product_name, v.vendor_name, p.product_price, oi.quantity
 from product p
-join vendor v on v.vendor_id = p.vendor_id
-join order_item oi on oi.product_id = p.product_id
+	join vendor v on v.vendor_id = p.vendor_id
+	join order_item oi on oi.product_id = p.product_id
 where oi.order_id = 20007
 order by vendor_name;
 
@@ -131,7 +132,7 @@ where cust_id in (
 -- Query 16
 select c.cust_name, c.cust_city, count(o.order_id) as total
 from [order] o
-right join customer c on c.cust_id = o.cust_id
+	right join customer c on c.cust_id = o.cust_id
 where c.cust_id in (
 	select cust_id
 	from customer
@@ -142,7 +143,7 @@ order by c.cust_name;
 -- works without sub-query
 select c.cust_name, c.cust_city, count(o.order_id) as total
 from [order] o
-right join customer c on c.cust_id = o.cust_id
+	right join customer c on c.cust_id = o.cust_id
 group by o.cust_id, c.cust_name, c.cust_city
 order by c.cust_name;
 
@@ -182,11 +183,14 @@ order by cust_name, cust_contact;
 
 -- Query 18
 drop view if exists vProductCustomer;
-create view vProductCustomer as
-select c.cust_id, c.cust_name, c.cust_contact, o.order_id, o.order_date, oi.item_price, oi.order_item, oi.quantity, oi.product_id
-from customer c
-inner join [order] o on o.cust_id = c.cust_id
-inner join order_item oi on oi.order_id = o.order_id;
+go
+create view vProductCustomer
+as
+	select c.cust_id, c.cust_name, c.cust_contact, o.order_id, o.order_date, oi.item_price, oi.order_item, oi.quantity, oi.product_id
+	from customer c
+		inner join [order] o on o.cust_id = c.cust_id
+		inner join order_item oi on oi.order_id = o.order_id;
+go
 
 select cust_name, cust_contact
 from vProductCustomer
@@ -202,20 +206,23 @@ values ('1000000006', 'the toy emporium','09-546-8552');
 
 -- Including null values
 drop view if exists vCustomerMailingLabel;
+go
 create view vCustomerMailingLabel as
 select cust_name, cust_address, concat(trim(isnull(cust_city, 'NULL')), ', ', cust_phone) as 'city/phone'
 from customer;
-
+go
 select * from vCustomerMailingLabel;
 
 -- excluding null values
 drop view if exists vCustomerMailingLabel;
+go
 create view vCustomerMailingLabel as
 select cust_name, cust_address, concat(trim(isnull(cust_city, 'NULL')), ', ', cust_phone) as 'city/phone'
 from customer
 where cust_address is not null
 and cust_city is not null
 and cust_phone is not null;
+go
 
 select * from vCustomerMailingLabel;
 
